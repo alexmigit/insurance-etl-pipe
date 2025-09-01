fd45a7c80eae4c8ebe14993ed40a10975251420b
Alex Migit <migit001@umn.edu>
2025-08-31 21:36:02 -0500

    Update claims intermediate model

--- dbt/models/intermediate/int_claims_enriched.sql
+++ dbt/models/intermediate/int_claims_enriched.sql
@@ -1,41 +1,41 @@
+{{ 
+  config(
+    materialized='view'
+  ) 
+}}
+
 with 
 
-base as (
+claims as (
 
-    select * from {{ ref('stg_claims') }}
+  select * from {{ ref('stg_claims') }}
 
 ),
 
-joined as (
+policies as (
 
-    select
-        c.claim_id,
-        c.policy_id,
-        c.claim_date,
-        c.claim_amount,
-        c.status,
-        p.policy_type,
-        p.premium_amount,
-        cust.customer_id,
-        cust.first_name,
-        cust.last_name,
-        a.agent_id,
-        a.agent_name,
-        a.region
-
-    from base c
-
-    left join {{ ref('stg_policy') }} p
-      on c.policy_id = p.policy_id
-
-    left join {{ ref('stg_customer') }} cust
-      on p.customer_id = cust.customer_id
-
-    left join {{ ref('stg_agent') }} a
-      on p.agent_id = a.agent_id
+  select * from {{ ref('int_policy_enriched') }}
 
 )
 
-select *
+select
+  c.claim_id,
+  c.claim_date,
+  c.claim_amount,
+  c.status,
+  p.policy_id,
+  p.policy_type,
+  p.premium_amount,
+  p.status as policy_status,
+  p.customer_id,
+  p.customer_name,
+  p.customer_address,
+  --p.customer_city,
+  --p.customer_state,
+  p.agent_id,
+  p.agent_name
 
-from joined
+from claims c
+
+left join policies p
+  on c.policy_id = p.policy_id
