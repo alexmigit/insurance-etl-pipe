6a10ceac145cbde4ea1caf588855c6ebba39d273
Alex Migit <migit001@umn.edu>
2025-09-05 15:18:26 -0500

    chore: update columns

--- dbt/models/staging/stg_agent.sql
+++ dbt/models/staging/stg_agent.sql
@@ -2,7 +2,15 @@ with
 
 source as (
 
-    select * from {{ source('raw', 'raw_agent') }}
+    select
+        agent_id,
+        first_name,
+        last_name,
+        email,
+        phone,
+        agency_name
+    
+    from {{ source('raw', 'raw_agent') }}
 
 ),
 
@@ -10,8 +18,10 @@ renamed as (
 
     select
         cast(agent_id as string) as agent_id,
-        agent_name,
-        region
+        first_name || ' ' || last_name as agent_name,
+        email as agent_email,
+        phone as agent_phone,
+        agency_name as agent_agency
         
     from source
 )
