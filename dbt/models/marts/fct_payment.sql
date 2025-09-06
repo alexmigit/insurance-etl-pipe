077e15010ae2262d878df894dc6880eae33fc2aa
Alex Migit <migit001@umn.edu>
2025-09-06 17:42:59 -0500

    feat: add payment fact model

---
+++ dbt/models/marts/fct_payment.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+select
+    payment_id,
+    amount,
+    payment_method,
+    policy_id,
+    policy_type,
+    premium_amount,
+    customer_id,
+    agent_id,
+    --payment_date
+from {{ ref('int_payment_enriched') }}
